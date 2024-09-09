cb378d8b1f4ae649208eab47fe00fcd08fbda7f6
Dias1c <dias.kappasov@gmail.com>
2024-09-09 18:18:16 +0500

    add: CExternalFunctions

---
+++ src/CFunctions/CExternalFunctions.ts
@@ -0,0 +1,26 @@
+import { CInternalFunctions } from "./CInternalFunctions";
+import { TFunc } from "./types";
+
+export class CExternalFunctions {
+  controller: CInternalFunctions;
+
+  constructor(_controller: CInternalFunctions) {
+    this.controller = _controller;
+  }
+
+  push(func: TFunc) {
+    this.controller.push(func);
+  }
+
+  remove(func: TFunc) {
+    this.controller.remove(func);
+  }
+
+  set(arr: TFunc[]) {
+    this.controller.set(arr);
+  }
+
+  clear() {
+    this.controller.clear();
+  }
+}
